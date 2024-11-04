ca4a2a32e0397c46ccd9f435167ffa4394a16356
Tematzuka8 <hyper.creative.tematzuka8@gmail,com>
2024-11-04 14:03:44 +0900

    vite+reactを導入

--- style.js
+++
@@ -1,9 +0,0 @@
-var codeElements = document.querySelectorAll(".codeText");
-codeElements.forEach(function (codeText) {
-    var processedText = codeText.innerText
-        .split('\n')
-        .filter(function (line) { return line.trim() !== ''; }) // 空行を除去
-        .map(function (line) { return line.trim(); }) // インデントを除去
-        .join('<br>'); // 改行を追加
-    codeText.innerHTML = processedText;
-});
